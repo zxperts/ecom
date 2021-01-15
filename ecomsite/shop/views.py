aa4599251bafd060bb4a22068ab61c2736ce3bb7
MUNSI Patrick <patrick.munsi@ethias.be>
2021-01-15 23:56:08 +0100

    Adding Cart Items To Database

--- ecomsite/shop/views.py
+++ ecomsite/shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Products
+from .models import Products, Order
 from django.core.paginator import Paginator
 
 # Create your views here.
@@ -28,5 +28,21 @@ def detail(request, id):
 
 
 def checkout(request):
+
+    if request.method == "POST":
+        items=request.POST.get('items',"")
+        name=request.POST.get('name',"")
+        email=request.POST.get('email',"")
+        address=request.POST.get('address',"")
+        city=request.POST.get('city',"")
+        state=request.POST.get('state',"")
+        zipcode=request.POST.get('zipcode',"")
+
+        order = Order(items=items, name=name, email=email, address=address, city=city, state=state , zipcode=zipcode)
+        order.save()
+
+
+
+
     return render(request, 'shop/checkout.html')
 
